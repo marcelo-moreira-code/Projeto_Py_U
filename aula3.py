4d7ac5a0af1c319cd00d76b780e11db9c09c872d
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-14 23:57:09 -0300

    Update aula3.py

--- aula3.py
+++ aula3.py
@@ -4,4 +4,11 @@ Tipo de tipagem = Dinâmica e Forte
 str string texto
 Strings são textos que estão dentro de aspas
 
+Tipos Primitivos : str/int/float/ bool
 """
+# Escape    \
+print('Ana \'Beatriz\'')
+
+# r
+print(r'Ana \'Beatriz\'')
+# r -> é usada para expressões regulares 
