d0d53879dc2800e89b841c359c311c35aa920ffb
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-14 23:46:36 -0300

    aulas 1,2,3

--- aula1.py
+++ aula1.py
@@ -7,3 +7,11 @@ print(f'Olá {nome}! Sua idade é {idade} anos, correto?')
 
 print('Ok')
 print('Tudo certo')
+
+'''
+DocStrings
+DocStrings não são comentários, e são lidas pelo interpretador do pyhton e guadadas na memória.
+Servem para escrever notas ou comentar seu cod como um texto
+'''
+# 
+print('Boa noite!')
---
+++ aula2.py
@@ -0,0 +1,8 @@
+# \r  \n -> CRLF = quebra linha
+print(23, 35) # Argumentos não nomeados dentro da função print
+print(55, 66)
+
+print(33, 44, sep='-', end='\r\n') # sep - separador           end=''
+print(11, 23, sep="*")
+print(42, 25, end='##\n')
+print('Boa noite')
