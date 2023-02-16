4b64a0201ed9449742c673058b4cf3b2c9042e97
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-16 00:00:21 -0300

    Create aula8.py

---
+++ aula8.py
@@ -0,0 +1,17 @@
+nome = 'Ana'
+sobrenome = 'Beatriz'
+idade = 25
+ano_nascimento = 1998
+maior_de_idade = (2023-(ano_nascimento)) >= 18
+altura_metros = 1.66 
+idade = 2023 - ano_nascimento
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
+print(f'{nome} {sobrenome} , nasceu em {ano_nascimento}, portanto possui {idade} anos de idade e uma altura de {altura_metros} m.')
