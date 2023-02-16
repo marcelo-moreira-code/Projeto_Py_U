ffd070cf25bd2bb7db3a3d8a19454fa3fda346a5
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-15 23:48:11 -0300

    a5,6,7

---
+++ aula6.py
@@ -0,0 +1,10 @@
+# Conversão de tipos , coerção
+# typr convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos str/int/float/bool 
+
+#print('1' + 1)
+print(float(int('1') + 2))
+
+# bool 
+#print(bool()) -> str vazia é False , e com algo True
---
+++ aula7.py
@@ -0,0 +1,3 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# O sinal = é o operador de atribuição. Ele é usado para atribuir um valor a um nome(variável).
+# variáveis -> minúsculas,números  e underlines 
