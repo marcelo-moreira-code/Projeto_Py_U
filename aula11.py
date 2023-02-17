e4abdd6d3b1ccfc548586c7f7e27b76cf2a10137
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-16 23:44:20 -0300

    Update aula11.py

--- aula11.py
+++ aula11.py
@@ -1,5 +1,8 @@
 # precedência dos operadores
-# 1 .(n+1)
+# 1 . (n+1)
 # 2. **
 # 3. * /  //  %
 # 4. + -
+# execução da esquerda para direita , de cima para baixo. 
+# Parênteses dentro de parênteses são executados do mais interno para o mais externos
+print((( 2 ** 3)* 2) * 3 - 20)
