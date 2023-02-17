42d252dec59fd96b2b2aa85b8acede74f4bdba88
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-16 23:51:28 -0300

    Create aula12.py

---
+++ aula12.py
@@ -0,0 +1,9 @@
+nome = 'Ana Valentina'
+altura = 1.64
+peso = 62
+imc = peso/(altura)**2
+cor_favorita = ...   # placeholder ... não gera erro
+
+print(f'''{nome} tem {altura} m de altura,
+pesa {peso} kg e seu imc é 
+{imc:.1f}''')
