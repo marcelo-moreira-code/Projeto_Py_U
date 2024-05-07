b4228d6ddfd5346a5e9f975c972108106833a04a
Marcelo S. Moreira <120063058+marcelo-moreira-code@users.noreply.github.com>
2024-05-07 12:02:32 -0400

    Update a39_Treasure _Island.py

--- Angela-Ud/unidade-3/a39_Treasure _Island.py
+++ Angela-Ud/unidade-3/a39_Treasure _Island.py
@@ -1,24 +1,25 @@
 print("Welcome to Treasure Island!")
 print("Your mission is to find the treasure.")
-choice = input("You're at a cross road. Where do you want to go? Type 'left' or 'right': ")
+choice = input("You're at a cross road. Where do you want to go? Type 'left' or 'right': ").lower()
 print(choice)
 #choice_left
 if choice == "left":
     choice_wait = input("You come to a lake. There is an island in the middle of the lake.\n Type 'wait' to wait for a boat.\n Or type 'swim' to swim across. ")
     if choice_wait == "wait":
-        color = input("You arrive at the island unharmed.\nThere is a house with 3 doors.\nOne red, one yellow and one blue.\nWhich colour dou you choose?")
+        color = input("You arrive at the island unharmed.\nThere is a house with 3 doors.\nOne red, one yellow and one blue.\nWhich colour dou you choose?").lower()
         if color == "blue" or "red":
             print("You enter a room of beasts. Game Over!")
         else:
             print("You Win!")    
     else:
-        print("Swing a across!")    
+        print("You got attacked by a angry trout. Game Over!")
+        #print()    
 
 
 
 
 #choice_right 
-if choice == "right":
-    print("You fail!")
+else:
+    print("You fell into a hole. Game Over!")
 
 
