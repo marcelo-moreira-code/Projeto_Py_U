23fe0036b56b537445a8a090c3448e490d8730ee
Marcelo S. Moreira <contatomarcelomoreira.code@gmail.com>
2024-07-07 18:52:52 -0300

    Create aula13.py

---
+++ otavio/aula13.py
@@ -0,0 +1,8 @@
+nome = str(input("Escreva um nome: ")).upper()
+"""if nome[-1] == "O":
+    print("Seu nome é masculino.")
+elif nome[-1] == "A":
+    print("Seu nome é feminino.")
+else:
+    print("Que porra de nome é esse??")"""
+print(nome[-1])
