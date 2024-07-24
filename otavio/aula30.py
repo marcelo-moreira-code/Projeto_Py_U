5a3b0ea25ee31b48450bc9256f7b77f09dec2d82
Marcelo S. Moreira <contatomarcelomoreira.code@gmail.com>
2024-07-24 13:21:02 -0300

    31

--- otavio/aula30.py
+++ otavio/aula30.py
@@ -8,9 +8,29 @@ Muitas condições no mesmo if (ruim)
 """
 
 velocidade = 61 # velocidade atual do carro
-local_carro = 150 # local em que o carro está na estrada
+local_carro = 101 # local em que o carro está na estrada
 
 
 RADAR_1 = 60 # velocidade máxima do radar 1
 LOCAL_1 = 100 # local onde o radar 1 está
 RADAR_RANGE = 1 # A distância onde o radar pega
+
+
+velocidade_carro_passou_radar1 = velocidade > RADAR_1 # no lugar   velocidade > RADAR_1
+
+carro_passou_radar_1 = local_carro >=  (LOCAL_1 - RADAR_RANGE)  and \
+     local_carro <= (LOCAL_1 + RADAR_RANGE)
+
+carro_multado_radar_1 = carro_passou_radar_1 and velocidade_carro_passou_radar1
+
+
+
+if velocidade_carro_passou_radar1:
+    print("Velocidade do carro passou do Radar 1")
+
+if carro_passou_radar_1:
+    print("Carro passou no radar 1")
+
+
+if carro_multado_radar_1:
+    print("Carro multado em radar 1")
